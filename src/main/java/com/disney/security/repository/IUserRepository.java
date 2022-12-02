4a63d6cbe9647641d78264c7ead1079836f3d742
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-02 18:18:56 -0300

    CA-25: Method findByUserNameOrEmail added in IUserRepository

--- src/main/java/com/disney/security/repository/IUserRepository.java
+++ src/main/java/com/disney/security/repository/IUserRepository.java
@@ -1,12 +1,14 @@
-package com.disney.repository;
+package com.disney.security.repository;
 
 import com.disney.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
-
+    Optional<User> findByUserNameOrEmail(String userName, String email);
 
 }
