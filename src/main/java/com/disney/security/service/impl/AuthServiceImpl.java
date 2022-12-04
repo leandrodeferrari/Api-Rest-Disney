f7993d2be41cb8edee2fb9a0e75f8032105ef6aa
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-04 15:32:17 -0300

    fix/CA-26: Creation method login in AuthServiceImpl

--- src/main/java/com/disney/security/service/impl/AuthServiceImpl.java
+++ src/main/java/com/disney/security/service/impl/AuthServiceImpl.java
@@ -1,14 +1,20 @@
 package com.disney.security.service.impl;
 
+import com.disney.security.dto.LoginInDto;
+import com.disney.security.dto.LoginOutDto;
 import com.disney.security.dto.RegisterInDto;
 import com.disney.security.dto.RegisterOutDto;
+import com.disney.security.jwt.JwtTokenProvider;
 import com.disney.security.mapper.IAuthMapper;
 import com.disney.security.model.Role;
 import com.disney.security.model.User;
+import com.disney.security.model.UserDetailsImpl;
 import com.disney.security.repository.IRoleRepository;
 import com.disney.security.repository.IUserRepository;
 import com.disney.security.model.enums.RoleEnum;
 import com.disney.security.service.IAuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,12 +29,18 @@ public class AuthServiceImpl implements IAuthService {
 
     private final IAuthMapper authMapper;
     private final IRoleRepository roleRepository;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
     private final IUserRepository userRepository;
+    private final AuthenticationManager authManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthServiceImpl(IAuthMapper authMapper, IRoleRepository roleRepository, IUserRepository userRepository){
+    public AuthServiceImpl(AuthenticationManager authManager, IAuthMapper authMapper, IRoleRepository roleRepository, IUserRepository userRepository, JwtTokenProvider jwtTokenProvider, UserDetailsServiceImpl userDetailsServiceImpl){
+        this.authManager = authManager;
         this.authMapper = authMapper;
         this.roleRepository = roleRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
         this.userRepository = userRepository;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
     }
 
     @Transactional
@@ -48,6 +60,19 @@ public class AuthServiceImpl implements IAuthService {
         userRepository.save(user);
 
         return authMapper.userToRegisterOutDto(user);
+
+    }
+
+    @Override
+    public LoginOutDto login(LoginInDto loginInDto) {
+
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(loginInDto.getUserNameOrEmail(), loginInDto.getPassword()));
+
+        final UserDetailsImpl userDetails = userDetailsServiceImpl.loadUserByUsername(loginInDto.getUserNameOrEmail());
+
+        String token = jwtTokenProvider.generateToken(userDetails);
+
+        return new LoginOutDto(loginInDto.getUserNameOrEmail(), token);
 
     }
 
