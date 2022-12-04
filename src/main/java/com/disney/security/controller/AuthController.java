14769c1586bc30c4f282af761eed86bbc9d52aee
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-04 15:32:49 -0300

    fix/CA-26: Creation endpoint login in AuthController

--- src/main/java/com/disney/security/controller/AuthController.java
+++ src/main/java/com/disney/security/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.disney.security.controller;
 
+import com.disney.security.dto.LoginInDto;
+import com.disney.security.dto.LoginOutDto;
 import com.disney.security.dto.RegisterInDto;
 import com.disney.security.dto.RegisterOutDto;
 import com.disney.security.service.IAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,7 @@ public class AuthController {
 
     private final IAuthService authService;
 
+    @Autowired
     public AuthController(IAuthService authService){
         this.authService = authService;
     }
@@ -25,6 +29,11 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<RegisterOutDto> register(@Valid @RequestBody RegisterInDto registerInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerInDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginOutDto> login(@Valid @RequestBody LoginInDto loginInDto){
+        return ResponseEntity.ok().body(authService.login(loginInDto));
     }
 
 }
